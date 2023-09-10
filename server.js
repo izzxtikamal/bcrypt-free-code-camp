dbca7768f4e32a81b0025f6fcbb311ffe4409070
Izzati Kamal <izzxtikamal@aikay.aikay>
2023-09-10 09:41:19 -0400

    bcrypt hash synchronous

--- server.js
+++ server.js
@@ -12,23 +12,22 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-    console.log(res);
-  });
-});
+//const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    //console.log(hash);
+  //bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    //console.log(res);
+  //});
+//});
 
+//bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    //console.log(hash);
+    //bcrypt.compare('passw0rd!', hash, (err, res) => {
+      //console.log(res); //true
+    //});
+  //});  
 
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
-    console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('passw0rd!', hash, (err, res) => {
-      console.log(res); //true
-    });
-  });  
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 //END_ASYNC
 
